b241179462bf2e06781d3207db742a93d6f6e5d0
noacohen2 <noaaq@post.bgu.ac.il>
2021-11-15 13:17:30 +0200

    hello noa

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main() {
+<<<<<<< HEAD
     std::cout << "Hello, lior!" << std::endl;
+=======
+    std::cout << "Hello, noa!" << std::endl;
+>>>>>>> hello noa
     return 0;
 }
