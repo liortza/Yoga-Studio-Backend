85d3b6e46c4d63092a839da2bdf22706ee719221
liorza <liortza@post.bgu.ac.il>
2021-11-21 11:38:35 +0200

    hii

--- src/PrintActionsLog.cpp
+++ src/PrintActionsLog.cpp
@@ -1,3 +1,9 @@
 #include "../include/Action.h"
 
+using namespace std;
+
 PrintActionsLog::PrintActionsLog() {}
+
+void PrintActionsLog::act(Studio &studio) {}
+
+std::string PrintActionsLog::toString() const {}
