a2c757423938a5242643b6cbb029a84597bb741b
noacohen2 <noaaq@post.bgu.ac.il>
2021-11-27 13:53:16 +0200

    cheap

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -45,21 +45,19 @@ CheapCustomer::CheapCustomer(std::string name, int id) : Customer(name, id) {}
 
 std::vector<int> CheapCustomer::order(const std::vector<Workout> &workout_options) {
     vector<int> workout_ids;
-    if (!workout_options.empty()) {
-        int cheapest = INT32_MAX;
-        int id;
-        Workout *cheapest_workout;
-        for (Workout W: workout_options) {
-            if (W.getPrice() < cheapest) {
-                cheapest = W.getPrice();
-                id = W.getId();
-                cheapest_workout = &W;
-            }
+    int cheapest = INT32_MAX;
+    int id;
+    string name;
+    for (Workout W: workout_options) { // assuming workout_options is not empty
+        if (W.getPrice() < cheapest) {
+            cheapest = W.getPrice();
+            id = W.getId();
+            name = W.getName();
         }
-        workout_ids = {id};
-        pay += cheapest; // assuming at least one workout
-        cout << getName() << " Is Doing " << cheapest_workout->getName() << endl;
     }
+    workout_ids = {id};
+    pay += cheapest; // assuming at least one workout
+    cout << getName() << " Is Doing " << name << endl;
     return workout_ids;
 }
 
